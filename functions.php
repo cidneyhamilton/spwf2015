4a99ee94add78eabb2f2142798b7f78a5dabde15
C. Hamilton <cidney@cidney.org>
2015-01-04 11:27:22 -0500

    Use serif type sheet for SPWF, additional styling.

--- functions.php
+++ functions.php
@@ -5,6 +5,8 @@ add_action( 'wp_enqueue_scripts', 'spwf_styles' );
 function spwf_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
 
+    wp_enqueue_style( 'serif-type', get_template_directory_uri().'/serif-type.css' );
+
     wp_enqueue_style( 'style', get_stylesheet_uri() );
 }
 
